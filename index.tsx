5334f53a0062dd76b6d3ad86399f890021a3433e
Ayutayam sutar <ayutayamsutar01@gmail.com>
2025-08-18 20:59:24 +0530

    main

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorkerRegistration from './services/serviceWorkerRegistration'; // 1. Import the service
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // 1. Import the service
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
