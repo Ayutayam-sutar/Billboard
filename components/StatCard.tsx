5831d0982b5be88158b1c4cbd5f47ad257298216
Ayutayam sutar <ayutayamsutar01@gmail.com>
2025-08-20 20:39:28 +0530

    main

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -12,7 +12,7 @@ interface StatCardProps {
 
 const StatCard = ({ icon, value, label, colorClass }: StatCardProps): React.ReactNode => {
   return (
-   <div className="bg-amber-50 backdrop-blur-sm p-4 sm:p-5 rounded-xl shadow-lg flex items-center space-x-4 sm:space-x-5 border border-amber-100 transition-all hover:border-amber-200 hover:scale-[1.02] duration-300">
+   <div className="bg-amber-50 pl-3 backdrop-blur-sm p-4 sm:p-5 rounded-xl shadow-lg flex items-center space-x-4 sm:space-x-5 border border-amber-100 transition-all hover:border-amber-200 hover:scale-[1.02] duration-300">
   <div className={`p-3 sm:p-4 rounded-lg ${colorClass} flex-shrink-0`}>
     {icon}
   </div>
