be1f2e56e6a870351693c6ddb90a6ed4eed8888d
Siddharath <siddharthkj55@gmail.com>
2025-08-21 09:55:10 +0530

    main

---
+++ android/app/src/main/java/com/juggernaut/billboardinspector/MainActivity.java
@@ -0,0 +1,5 @@
+package com.juggernaut.billboardinspector;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.juggernaut.billboardinspector',
+  appName: 'Juggernaut_ITER_Billboard Inspector',
+  webDir: 'dist'
+};
+
+export default config;
